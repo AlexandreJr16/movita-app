f5afc3f2439484102c2376dc561a798e7ca115c2
AlexandreJr16 <alexandrep.souzzza@gmail.com>
2023-11-15 19:35:54 -0400

    Atualizadno Width do carrossel

--- src/components/Login/Carrossel/Carrossel.tsx
+++ src/components/Login/Carrossel/Carrossel.tsx
@@ -1,18 +1,14 @@
-import React from "react";
-import {
-  SafeAreaView,
-  View,
-  StyleSheet,
-  Image,
-  StatusBar,
-  Text,
-} from "react-native";
+import React, { useState } from "react";
+import { View, Image, StatusBar, Text, Dimensions } from "react-native";
 import Carousel from "react-native-snap-carousel";
 
-const Item = ({ item }: { item: { image: string } }) => {
+const SLIDER_WIDTH = Dimensions.get("window").width;
+const ITEM_WIDTH = SLIDER_WIDTH / 2;
+
+const Item = ({ item }: { item: { text: string } }) => {
   return (
-    <View style={styles.slide}>
-      <Image source={{ uri: item.image }} style={{ width: 300, height: 300 }} />
+    <View style={{ width: "100%", backgroundColor: "#1f1f1f" }}>
+      <Text style={{ color: "#fff" }}>{item.text}</Text>
     </View>
   );
 };
@@ -20,78 +16,38 @@ const Item = ({ item }: { item: { image: string } }) => {
 export const Carrossel = () => {
   const carousel = [
     {
-      image: "https://i.imgur.com/SsJmZ9jl.jpg",
+      text: "Text 1",
     },
     {
-      image: "https://i.imgur.com/5tj6S7Ol.jpg",
+      text: "Text 2",
     },
     {
-      image: "https://i.imgur.com/pmSqIFZl.jpg",
+      text: "Text 3",
     },
     {
-      image: "https://i.imgur.com/cA8zoGel.jpg",
+      text: "Text 4",
     },
     {
-      image: "https://i.imgur.com/pewusMzl.jpg",
+      text: "Text 5",
     },
     {
-      image: "https://i.imgur.com/l49aYS3l.jpg",
+      text: "Text 6",
     },
   ];
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <StatusBar
-          translucent={true}
-          backgroundColor={"rgba(0, 0, 0, 0.3)"}
-          barStyle={"light-content"}
-        />
-        <Text style={styles.title}>Carrosel</Text>
-        <Carousel
-          data={carousel}
-          renderItem={Item}
-          sliderWidth={300}
-          itemWidth={300}
-          inactiveSlideScale={0.95}
-          inactiveSlideOpacity={1}
-          enableMomentum={true}
-          activeSlideAlignment={"start"}
-          activeAnimationType={"spring"}
-        />
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text style={{ marginBottom: 100 }}>Carrosel</Text>
+      <Carousel
+        data={carousel}
+        renderItem={Item}
+        layout={"default"}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}
+        loop={true}
+      />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: "#1a1917",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#B721FF",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  gradient: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  slide: {
-    backgroundColor: "floralwhite",
-    borderRadius: 5,
-    height: 250,
-  },
-  title: {
-    fontSize: 60,
-    paddingVertical: 20,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-  },
-  scrollview: {
-    flex: 1,
-  },
-});
 
 export default Carrossel;
