ac0cb462bbe6b17623f089dde831dc9eacf5b7f3
AlexandreJr16 <alexandrep.souzzza@gmail.com>
2023-11-16 22:41:06 -0400

    Commit da modificação em Carrossel.tsx

--- src/components/Login/Carrossel/Carrossel.tsx
+++ src/components/Login/Carrossel/Carrossel.tsx
@@ -1,50 +1,54 @@
 import React, { useState } from "react";
 import { View, Image, StatusBar, Text, Dimensions } from "react-native";
 import Carousel from "react-native-snap-carousel";
+import GaveteiroAzul from "../../../assents/Login/GaveteiroAzul";
+import GaveteiroCreme from "../../../assents/Login/GaveteiroCreme";
+import GaveteiroVermelho from "../../../assents/Login/GaveteiroVermelho";
+import Texto from "../../texto/Texto";
 
 const SLIDER_WIDTH = Dimensions.get("window").width;
 const SLIDER_HEIGHT = Dimensions.get("window").height;
 
 const ITEM_WIDTH = SLIDER_WIDTH / 2;
-const ITEM_HEIGHT = SLIDER_HEIGHT / 3.5;
+const ITEM_HEIGHT = SLIDER_HEIGHT / 3.2;
 
-const Item = ({ item }: { item: { color: string } }) => {
+const Item = ({ item }: { item: { color: string; img: any } }) => {
   return (
     <View
       style={{
         width: "100%",
         backgroundColor: item.color,
         height: ITEM_HEIGHT,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20,
       }}
-    ></View>
+    >
+      {item.img}
+    </View>
   );
 };
 
 export const Carrossel = () => {
   const carousel = [
     {
-      color: "#1f1f1f",
+      color: "#A64029",
+      img: <GaveteiroVermelho />,
     },
     {
-      color: "#1f1f1f",
+      color: "#36A5BF",
+      img: <GaveteiroAzul />,
     },
+
     {
-      color: "#1f1f1f",
-    },
-    {
-      color: "#1f1f1f",
-    },
-    {
-      color: "#1f1f1f",
-    },
-    {
-      color: "#1f1f1f",
+      color: "#BF9969",
+      img: <GaveteiroCreme />,
     },
   ];
 
   return (
     <View>
-      <Text style={{ marginBottom: 100 }}>Carrosel</Text>
       <Carousel
         data={carousel}
         renderItem={({ item }) => <Item item={item} />}
@@ -52,7 +56,9 @@ export const Carrossel = () => {
         sliderWidth={SLIDER_WIDTH}
         sliderHeight={SLIDER_HEIGHT}
         itemWidth={ITEM_WIDTH}
+        itemHeight={ITEM_HEIGHT}
         loop={true}
+        inactiveSlideOpacity={1}
       />
     </View>
   );
