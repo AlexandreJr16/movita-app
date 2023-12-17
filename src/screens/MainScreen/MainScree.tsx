f85c5ecb9711b2f29528252e840da2c249b0f83e
Ihago Nunes <ihago2nunes@gmail.com>
2023-12-17 01:10:51 -0400

    criação arquivo navbar

--- src/screens/MainScreen/MainScree.tsx
+++ src/screens/MainScreen/MainScree.tsx
@@ -5,23 +5,40 @@ import AuthContext from "../../contexts/auth";
 import styles from "./styles";
 import Texto from "../../components/texto/Texto";
 import Logo from "../../components/Logo/Logo";
+import Carrossel from "../../components/Login/Carrossel/Carrossel";
+import NavBar from "../../components/NavBar/NavBar";
 
 export default function MainScrenn() {
   const { token, user } = useContext(AuthContext);
   const { nome } = user;
   return (
     <View style={styles.background}>
-      <SafeAreaView>
-        <Logo color="#FFFFFF" size="50"></Logo>
-        <View>
-          <Texto weight="bold" style={styles.titleMessage}>
-            Olá, {nome}
-          </Texto>
-          <Texto weight="bold" style={styles.subtitleMessage}>
-            Seja bem-vindo(a) ao Movita!
-          </Texto>
+
+        <View style={styles.topPage}>
+          <SafeAreaView>
+            <View style={styles.logo}>
+              <Logo color="#FFFFFF" size="32"/>
+            </View>
+            
+            
+            <View style={styles.welcome}>
+              <View style={styles.message}>
+                <Texto weight="bold" style={styles.titleMessage}>
+                  Olá, {nome}!
+                </Texto>
+                <Texto weight="bold" style={styles.subtitleMessage}>
+                  Seja bem-vindo ao Movita!
+                </Texto>
+              </View>
+      
+              <View style={styles.photo}></View>
+            </View>
+          </SafeAreaView>
         </View>
-      </SafeAreaView>
+
+        <NavBar/>
+
+
     </View>
   );
 }
--- src/screens/MainScreen/styles.ts
+++ src/screens/MainScreen/styles.ts
@@ -4,7 +4,35 @@ const styles = StyleSheet.create({
     background: {
         width: '100%',
         height: '100%',
-        backgroundColor: '#1F1F1F'
+        backgroundColor: '#414141'
+    },
+
+    topPage: {
+        backgroundColor: '#1F1F1F',
+        borderBottomLeftRadius: 50,
+        paddingHorizontal: 20,
+        display: 'flex',
+        flexDirection: 'column'
+    },
+
+    logo :{
+        width: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+    },
+
+    welcome: {
+        width: '100%',
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    message: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 4,
     },
 
     titleMessage :{
@@ -15,8 +43,20 @@ const styles = StyleSheet.create({
     subtitleMessage :{
         fontSize: 20,
         color: '#FFFFFF' 
-    }
+    },
 
+    photo : {
+        width: 65,
+        height: 65,
+        borderRadius: 33,
+        backgroundColor: '#FFFFFF',
+    },
+
+    navbar : {
+        display: 'flex',
+        height: 136,
+        backgroundColor: 'FF0000',
+    }
 })
 
 export default styles;
