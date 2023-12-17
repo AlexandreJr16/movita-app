bf13bb1bb7d0bb2f89166bc80cfe449f95fea0e6
AlexandreJr16 <alexandrep.souzzza@gmail.com>
2023-12-17 15:02:14 -0400

    Mudanças

--- src/components/Main/Header/index.tsx
+++ src/components/Main/Header/index.tsx
@@ -12,34 +12,26 @@ const HeaderMain = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <SafeAreaView style={styles.headerCard}>
-      <Logo color="#FFFFFF" size="35" style={styles.logo}></Logo>
-      <View style={styles.infoContainer}>
-        <View style={styles.welcome}>
-          <Texto weight="bold" style={styles.titleMessage}>
-<<<<<<< HEAD
-            {/* Olá, {user.nome} */}
-            {user ? `Olá, ${user.nome}` : "Olá, Cliente"}
-=======
-            {/*Olá, {user.nome} */}
-            Olá, Alexandre
->>>>>>> 21e718b555e6653a611f48fb01fc5af353865bce
-          </Texto>
-          <Texto weight="bold" style={styles.subtitleMessage}>
-            Seja bem-vindo(a) ao Movita!
-          </Texto>
+    <View style={styles.headerCard}>
+      <SafeAreaView style={styles.headerCard}>
+        <Logo color="#FFFFFF" size="35" style={styles.logo}></Logo>
+        <View style={styles.infoContainer}>
+          <View style={styles.welcome}>
+            <Texto weight="bold" style={styles.titleMessage}>
+              {/* Olá, {user.nome} */}
+              {user ? `Olá, ${user.nome}` : "Olá, Cliente"}
+            </Texto>
+            <Texto weight="bold" style={styles.subtitleMessage}>
+              Seja bem-vindo(a) ao Movita!
+            </Texto>
+          </View>
+          {/* <ImagemBuffer imgBuffer={user.img} style={styles.img} /> */}
+          {user.img ? (
+            <ImagemBuffer imgBuffer={user.img} style={styles.img} />
+          ) : null}
         </View>
-<<<<<<< HEAD
-        {/* <ImagemBuffer imgBuffer={user.img} style={styles.img} /> */}
-        {user.img ? (
-          <ImagemBuffer imgBuffer={user.img} style={styles.img} />
-        ) : null}
-=======
-         {/*<ImagemBuffer imgBuffer={user.img} style={styles.img} />*/
-         }
->>>>>>> 21e718b555e6653a611f48fb01fc5af353865bce
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </View>
   );
 };
 
